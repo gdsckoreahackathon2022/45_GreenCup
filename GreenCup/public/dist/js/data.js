260d3b2709497c1d5d3fe04f88403bb62a272920
Blue-Kite <bluelemon112@gmail.com>
2022-02-05 10:51:00 +0900

    Greecup최종

---
+++ GreenCup/public/dist/js/data.js
@@ -0,0 +1,34 @@
+var data = [
+    {
+        title: "서울",
+        content: "스타벅스 광화문R점",
+        blackcup: "8",
+        greencup: "3",
+        lat: 37.571359,
+        lng: 126.976308,
+    },
+    {
+        title: "서울",
+        content: "이디야 명동역",
+        blackcup: "2",
+        greencup: "0",
+        lat: 37.560997,
+        lng: 126.983227,
+    },
+    {
+        title: "서울",
+        content: "빽다방 서울숲역점",
+        blackcup: "11",
+        greencup: "0",
+        lat: 37.546570,
+        lng: 127.044835,
+    },
+    {
+        title: "서울",
+        content: "메가커피 강남역점",
+        blackcup: "1",
+        greencup: "2",
+        lat: 37.498801,
+        lng: 127.027338,
+    },
+];
---
+++ GreenCup/public/startup.bat
@@ -0,0 +1,2 @@
+cd C:\Users\user\Desktop\project\GreenCup\public
+python -m http.server 8000
